5b67ed7208cb792e5487b6aca6008a2388818a3e
Clems-web <clement.servais1@gmail.com>
2020-12-03 13:05:31 +0100

    exo 1 finito et 2 50%

--- exo 2/app.js
+++ exo 2/app.js
@@ -1,12 +1,20 @@
 let liste = document.getElementById("liste-commissions");
 let input = document.getElementById("ingredients");
 let bouton = document.getElementById("bouton");
+let boutonSupp = document.getElementById("boutonsupp");
 
 
 bouton.addEventListener("click", function () {
     let creation = document.createElement("li");
     creation.innerHTML = input.value;
     liste.appendChild(creation);
+})
+
+boutonSupp.addEventListener("click", function () {
+    let elements = document.querySelectorAll("#liste-commissions > li");
+    if (elements.length > 0) {
+        elements[elements.length - 1].remove();
+    }
 })
 
 
